e8d352c2d231a0587e72205b51d8cff700f5369e
objelisks <objelisks@gmail.com>
2024-02-09 17:07:53 -0800

    Display lat,lng, center on LA

--- app/src/components/Map.tsx
+++ app/src/components/Map.tsx
@@ -1,11 +1,13 @@
 import mapboxgl from 'mapbox-gl'
-import { useEffect, useRef } from 'preact/hooks'
+import { useEffect, useRef, useState } from 'preact/hooks'
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoib2JqZWxpc2tzIiwiYSI6ImNsc2ZjOGtoeDBpMnIyd3BxNW8wazMwY3gifQ.oV7-f9BcvLgOHxgUKuQ9cw'
 
 export function Map() {
   const mapRef = useRef(null)
   const mapContainer = useRef(null)
+  const [[lng, lat], setCenter] = useState([-118.2024, 33.9881])
+  const [zoom, setZoom] = useState(9)
 
   // initialize the map on page load
   useEffect(() => {
@@ -13,12 +15,20 @@ export function Map() {
     mapRef.current = new mapboxgl.Map({
       container: mapContainer.current, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      center: [lng, lat], // starting position [lng, lat]
+      zoom: zoom, // starting zoom
+    })
+    mapRef.current.on('move', () => {
+      const newCenter = mapRef.current.getCenter()
+      setCenter([newCenter.lng.toFixed(4), newCenter.lat.toFixed(4)])
+      setZoom(mapRef.current.getZoom().toFixed(2))
     })
   })
 
   return (
-    <div ref={mapContainer} className="map-container"></div>
+    <>
+      <div ref={mapContainer} className="map-container"></div>
+      <div>{`lng: ${lng}, lat: ${lat}, zoom: ${zoom}`}</div>
+    </>
   )
 }
