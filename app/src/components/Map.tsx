cb3892beaa4e3e284cd1c3fcc4ffd3296346051b
objelisks <objelisks@gmail.com>
2024-02-10 10:40:46 -0800

    Add sources and layers to map

--- app/src/components/Map.tsx
+++ app/src/components/Map.tsx
@@ -1,5 +1,6 @@
 import mapboxgl from 'mapbox-gl'
 import { useEffect, useRef, useState } from 'preact/hooks'
+import LACountyBoundary from '..\assets\LA_County_Boundary_Feature_Layer.geojson'
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoib2JqZWxpc2tzIiwiYSI6ImNsc2ZjOGtoeDBpMnIyd3BxNW8wazMwY3gifQ.oV7-f9BcvLgOHxgUKuQ9cw'
 
@@ -12,16 +13,61 @@ export function Map() {
   // initialize the map on page load
   useEffect(() => {
     if (mapRef.current) return
-    mapRef.current = new mapboxgl.Map({
+    const map = mapRef.current = new mapboxgl.Map({
       container: mapContainer.current, // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      style: 'mapbox://styles/mapbox/dark-v11', // style URL
       center: [lng, lat], // starting position [lng, lat]
       zoom: zoom, // starting zoom
     })
-    mapRef.current.on('move', () => {
-      const newCenter = mapRef.current.getCenter()
+
+    map.on('move', () => {
+      const newCenter = map.getCenter()
       setCenter([newCenter.lng.toFixed(4), newCenter.lat.toFixed(4)])
-      setZoom(mapRef.current.getZoom().toFixed(2))
+      setZoom(map.getZoom().toFixed(2))
+    })
+
+    map.on('load', async () => {
+      // load the custom data
+      const LACountyBoundary = await (await fetch('/LA_County_Boundary_Feature_Layer.geojson')).json()
+      map.addSource('la-county-boundary', {
+        type: 'geojson',
+        data: LACountyBoundary
+      })
+
+      map.addLayer({
+        id: 'la-county-boundary',
+        type: 'line',
+        source: 'la-county-boundary',
+        paint: {
+          'line-color': 'hsla(114, 95%, 44%, 50%)'
+        }
+      },
+      'road-label-simple')
+
+      const JENIDataset = await (await fetch('Justice_Equity_Need_Index_(zip_code).geojson')).json()
+      console.log(JENIDataset)
+      map.addSource('jeni-dataset', {
+        type: 'geojson',
+        data: JENIDataset
+      })
+
+      map.addLayer({
+        id: 'jeni-dataset',
+        type: 'fill',
+        source: 'jeni-dataset',
+        paint: {
+          'fill-color': [
+            "interpolate",
+            ["linear"],
+            ["get", "jenipctl"],
+            0,
+            "hsla(0, 0%, 0%, 0)",
+            100,
+            "hsl(15, 90%, 23%)"
+          ]
+        }
+      },
+      'water')
     })
   })
 
